5225fd7b337c394061caca7d2e838bd95bbed3a0
ceey-esri <c.eyrond@esri.de>
2026-08-17 13:05:38 +0200

    Erste Version: Karte mit OAuth-Setup

---
+++ app.js
@@ -0,0 +1,6 @@
+// Hier kommt in den nächsten Schritten die Logik rein:
+// - Vorhaben-Typ-Auswahl
+// - Kriterien-Panel
+// - Klick-Handler + geometryEngine-Analyse
+
+console.log("App geladen – Karte sollte jetzt sichtbar sein.");
